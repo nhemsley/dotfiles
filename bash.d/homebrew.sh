9b68bdbc399eff6f3ed16c5df5a239ee8474da9b
Nick Hemsley <nick.hems@gmail.com>
2020-07-16 22:29:49 +1000

    shellenv homebrew

--- bash.d/homebrew.sh
+++ bash.d/homebrew.sh
@@ -1,1 +1,3 @@
 export HOMEBREW_BUNDLE_FILE=$HOME/.Brewfile
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
