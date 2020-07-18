0e0c130994d7ebe2e0f4d1063dd51eb890fc8006
Nick Hemsley <nick.hems@gmail.com>
2020-07-18 20:39:44 +1000

    move brew env into homebrew.sh

--- bash.d/homebrew.sh
+++ bash.d/homebrew.sh
@@ -1,3 +1,6 @@
 export HOMEBREW_BUNDLE_FILE=$HOME/.Brewfile
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+. $(brew env source-file)
+
