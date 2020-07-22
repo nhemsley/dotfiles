0799166cf87ccea1c0ae30dd945cb65be6547943
Nick Hemsley <nick.hems@gmail.com>
2020-07-22 21:31:16 +1000

    various installs

---
+++ script/install.sh
@@ -0,0 +1,21 @@
+sudo apt install $(cat ~/.dotfiles/config/apt-installs)
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+. ~/.dotfiles/homebrew.sh
+
+brew install rcm
+
+rcup
+
+
+. ~/.dotfiles/config/snap-installs
+
+mkdir ~/.installs
+brew tap nhemsley/homebrew-env
+brew install homebrew-env
+
+. ~/.dotfiles/config/snap-installs
+
+brew bundle
+
