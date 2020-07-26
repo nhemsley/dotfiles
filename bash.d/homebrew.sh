7b27e0fdd74a35f1cdaa67f6d320e352c94e52eb
Nick Hemsley <nick.hems@gmail.com>
2020-07-26 14:22:13 +1000

    tell homebrew not to update automatically

--- bash.d/homebrew.sh
+++ bash.d/homebrew.sh
@@ -1,4 +1,6 @@
+export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_BUNDLE_FILE=$HOME/.Brewfile
+
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
