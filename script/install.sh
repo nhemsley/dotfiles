d399d6f98cd22595a7c1b402492b6befe9971cdd
Nick Hemsley <nick.hems@gmail.com>
2020-08-18 09:37:22 +1000

    environments use dirs, not git branches

--- script/install.sh
+++ script/install.sh
@@ -3,9 +3,9 @@
 #git clone https://github.com/nhemsley/dotfiles.git $DOTFILES
 
 
-DOTFILES_ROOT = ~/.dotfiles
-DOTFILES_CONFIG = $DOTFILES_ROOT/config
-DOTFILES_ENVIRONMENTS=$(ls $DOTFILES/config/environments/)
+DOTFILES_ROOT=~/.dotfiles
+DOTFILES_CONFIG=$DOTFILES_ROOT/config
+DOTFILES_ENVIRONMENTS=$(ls $DOTFILES_CONFIG/environments/)
 
 echo "Available setup environments:"
 echo ""
@@ -22,8 +22,14 @@ read -r DOTFILES_USE_ENVIRONMENT
 
 echo $DOTFILES_USE_ENVIRONMENT > ~/.dotfiles/config/environment
 
-DOTFILES_CURRENT_ENVIRONMENT=$(cat $DOTFILES_CONFIG/environment)
-sudo apt install -y $(cat $DOTFILES_CONFIG/environments/$DOTFILES_CURRENT_ENVIRONMENT)
+DOTFILES_CURRENT_ENV=$(cat $DOTFILES_CONFIG/environment)
+DOTFILES_CURRENT_ENV_DIR=$DOTFILES_CONFIG/environments/$DOTFILES_CURRENT_ENV
+
+[ -f $DOTFILES_CURRENT_ENV_DIR/apt-installs ] && sudo apt install -y $(cat $DOTFILES_CURRENT_ENV_DIR/apt-installs)
+
+[ -f $DOTFILES_CURRENT_ENV_DIR/snap-installs ] && . $DOTFILES_CURRENT_ENV_DIR/snap-installs
+
+exit
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
